c48abf4e6e5ca9919a31799831ff17dc78a709c1
riodwimulyono0512-sudo <riodwimulyono0512@gmail.com>
2026-04-23 18:12:56 +0700

    4

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -8,3 +8,33 @@ public:
     int noMhs;
     Node *next;    
 };
+
+class LinkedList
+{
+    Node *START;
+
+public:
+    LinkedList()
+    {
+        START = NULL;
+    }
+
+    void addNode()
+    {
+        int nim;
+        cout << "nMasukkan Nomor Mahasiswa : ";
+        cin >> nim;
+
+        Node *nodeBaru = new Node;
+        nodeBaru->noMhs = nim;
+
+        if (START == NULL || nim <= START->noMhs)
+        {
+            if (START != NULL && nim == START->noMhs)
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+        }
+    }
+}
