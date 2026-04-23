ffdacd0bd050ef27a08733ff49196f3d97fcf491
riodwimulyono0512-sudo <riodwimulyono0512@gmail.com>
2026-04-23 18:44:16 +0700

    10

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -96,6 +96,21 @@ public:
 
     void traverse()
     {
-        
+      if (listEmpty())
+      {
+        cout << "\nList Kosong\n";
+      }
+      else
+      {
+        cout << "\nData di dalam list adalah:\n";
+        Node *currentNode = START;
+
+        while (currentNode != NULL)
+        {
+            cout << currentNode->noMhs << endl;
+            currentNode = currentNode->next;
+        }
+        cout << endl;
+      }  
     }
-}
+};
