06b056620763897c324bce5209ed21887af5b396
riodwimulyono0512-sudo <riodwimulyono0512@gmail.com>
2026-04-23 18:31:31 +0700

    8

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -61,6 +61,26 @@ public:
 
     bool listEmpty()
     {
+        return (START == NULL);
+    }
+
+    bool search(int nim, Node *&previous, Node *&current)
+    {
+        previous = START;
+        current = START;
+
+        while (current != NULL && nim != current->noMhs)
+        {
+            previous = current;
+            current = current->next;
+        }
+
+        return (current != NULL);
+    }
+
+    bool delNode(int nim)
+    {
+        Node *current, *previous;
         
     }
 }
